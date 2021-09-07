0bc2b8c2cd1a58f0e83525ad8707eaa67063b474
maxifernandez1999 <fmaximiliano443@gmail.com>
2021-09-07 14:43:09 -0300

    lastcommit:9-7

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -11,9 +11,12 @@ import { UsersService } from 'src/app/services/users.service';
 export class LoginComponent implements OnInit {
 
   users:User[] = [];
+
   passwordSignIn:any = '';
   emailSignIn:any = '';
+
   isRegister:boolean = false;
+
   nameSignUp:any;
   emailSignUp:any;
   passwordSignUp:any;
@@ -21,6 +24,7 @@ export class LoginComponent implements OnInit {
   @ViewChild('asContainer') container:any;
   @ViewChild('asMessage') message:any;
   @ViewChild('asSpinner') spinner:any;
+  @ViewChild('message') messageH3:any;
 
   constructor(private renderer2:Renderer2,
               private rutas:Router,
@@ -38,47 +42,69 @@ export class LoginComponent implements OnInit {
    }
 
   signIn(){
+    this.existSignIn();
+    this.showMessageNotRegister();
+  }
+  existSignIn(){
     this.users.forEach(user => {
-        if (user.email == this.emailSignIn
-          && user.password == this.passwordSignIn) {
-            this.isRegister = true;
-        }
+      if (user.email == this.emailSignIn
+        && user.password == this.passwordSignIn) {
+          this.isRegister = true;
+      }
     });
-    
-    this.showMessage();
+  }
+  existSignUp(){
+    this.users.forEach(user => {
+      if (user.email == this.emailSignUp
+        && user.password == this.passwordSignUp) {
+          this.isRegister = true;
+      }
+    });
   }
   ShowSpinner(){
       const elementSpinner = this.spinner.nativeElement;
       setTimeout(()=>{ 
         this.renderer2.setStyle(elementSpinner,'display','none');
       }, 2000);
-      this.renderer2.setStyle(elementSpinner,'display','block');
-      
-      
+      this.renderer2.setStyle(elementSpinner,'display','block');    
   }
   signUp(){
-    const date:Date = new Date();
-    let dateFormat:string = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-    this.userService.addUser({
-      name: this.nameSignUp,
-      password: this.passwordSignUp,
-      email: this.emailSignUp,
-      fechaIngreso: dateFormat
-    });
-    this.rutas.navigate(['home']);
+    this.existSignUp();
+    this.showMessageExist();
 
   }
-  showMessage(){
+  createMessage(message:string){
+    const elementMessage = this.messageH3.nativeElement;
+    this.renderer2.setProperty(elementMessage,'innerHTML',message);
+  }
+  showMessageExist(){
+    if (this.isRegister == true) {
+      this.ShowSpinner();
+      const elementMessage = this.message.nativeElement;
+      this.createMessage('El usuario ya existe! Pruebe con iniciar sesion')
+      this.renderer2.addClass(elementMessage,"visible"); 
+    }else{
+      const date:Date = new Date();
+      let dateFormat:string = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+      this.userService.addUser({
+        name: this.nameSignUp,
+        password: this.passwordSignUp,
+        email: this.emailSignUp,
+        fechaIngreso: dateFormat
+      });
+      this.rutas.navigate(['home']);
+    }
+  }
+  showMessageNotRegister(){
     if (this.isRegister == false) {
       this.ShowSpinner();
       const elementMessage = this.message.nativeElement;
-      this.renderer2.addClass(elementMessage,"visible");
-      
+      this.renderer2.addClass(elementMessage,"visible"); 
     }else{
       this.rutas.navigate(['home']);
     }
-    
   }
+  
   signUpButton(){
     const elementContainer = this.container.nativeElement;
     this.renderer2.addClass(elementContainer,"right-panel-active");
